a418911181deac17188584decaa4947ef90c3b07
eladyarkoni <eladyarkoni@gmail.com>
2011-02-16 23:21:53 +0200

    last commit

---
+++ JavaStudyEnterpriseCatalogClient/src/java/javastudyproject/client/catalog/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javastudyproject.client.catalog;
+
+import javastudyproject.service.CatalogMessageService;
+import javastudyproject.service.CategoryOps;
+import javastudyproject.service.OrderOps;
+import javastudyproject.service.ProductsOps;
+import javastudyproject.service.UserOps;
+import javax.ejb.EJB;
+
+
+
+/**
+ *
+ * @author EladYarkoni
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    @EJB
+    static CatalogMessageService catalogService;
+    @EJB
+    static CategoryOps categoryService;
+
+    public static void main(String[] args) {
+
+        try
+        {
+             new CatalogUserScreen();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Catched an exeption: " + e.getMessage());
+        }
+    }
+
+
+}
